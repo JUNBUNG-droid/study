ebc86b8a804ee85f5d2c20adf1b67cd88f82c757
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-22 21:38:21 +0900

    Title: 세 실수의 반올림, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251122/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+    double b;
+    double c;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    cout << fixed;
+    cout.precision(3);
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c;
+    return 0;
+}
