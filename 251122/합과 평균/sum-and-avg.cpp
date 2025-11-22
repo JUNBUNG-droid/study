b25ec4415583ec99fda1eb423496d11562fc257c
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-22 21:51:59 +0900

    Title: 합과 평균, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251122/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cin >> a >> b;
+
+    double total = a+b;
+    cout << a+b << " " << total/2;
+    return 0;
+}
