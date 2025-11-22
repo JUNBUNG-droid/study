304e71a9cbd9baee74fe8695ab257df272b1efb6
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-23 01:00:29 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251123/문자열 출력하기/print-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int i;
+    cin >> n;
+    for (i=0; i<n; i++){
+        cout << "LeebrosCode\n";
+    }
+    return 0;
+}
