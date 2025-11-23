7dcd0c0b9ab43f6835e8a8dc7c17919433e1a09a
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-24 00:47:27 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251124/점수 비교/score-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int c, d;
+    cin >> c >> d;
+    if (a>c && b>d){
+        cout << 1;
+    }
+    else{
+        cout << 0;
+    }
+    // Please write your code here.
+    return 0;
+}
