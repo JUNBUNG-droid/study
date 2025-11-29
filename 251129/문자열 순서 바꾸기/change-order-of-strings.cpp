c96e1241b59f5afb7340d0f3e2fc4fa2a0ca32d4
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-29 17:37:31 +0900

    Title: 문자열 순서 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251129/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s, t;
+    cin >> s >> t;
+    cout << t << "\n";
+    cout << s;
+    return 0;
+}
