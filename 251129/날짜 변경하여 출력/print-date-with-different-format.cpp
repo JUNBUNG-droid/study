dbfcc1396a6d3d3d43375a0d9e2abadeb0459114
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-29 17:57:57 +0900

    Title: 날짜 변경하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251129/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int m,d,y;
+    char a;
+    cin >> y >> a >> m >> a >> d;
+    cout << m << '-' << d << '-' << y;
+    return 0;
+}
