e3ab9c392d6e4740e77f3cede83f59e00d6c43c3
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-30 22:31:17 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251130/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin >> a >> b >> c;
+    int max=a;
+    if (max<b){
+        max=b;
+    }
+    if (max<c){
+        max=c;
+    }
+    cout << max;
+    return 0;
+}
