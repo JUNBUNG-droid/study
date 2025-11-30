c0ca10afc848ba3f61c33583bee632707334e760
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-30 16:46:36 +0900

    Title: 삼항연산자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251130/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    char b = a==1? 't' : 'f';
+    cout << b;
+    return 0;
+}
