1de98496d3b8c3a049edbed2394ce38eee4713e8
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-30 22:54:31 +0900

    Title: 중앙값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251130/중앙값 구하기/find-the-median.cpp
+++ 251130/중앙값 구하기/find-the-median.cpp
@@ -5,13 +5,13 @@ int main() {
     // Please write your code here.
     int a,b,c;
     cin >> a >> b >> c;
-    if(a>b && a<c){
+    if(a>b && a<c || a>c && a<b){
         cout << a;
     }
-    else if(b>a && b<c){
+    else if(b>a && b<c || b>c && b<a){
         cout << b;
     }
-    else{
+    else if(c>a && c<b || c>b && c<a){
         cout << c;
     }
     return 0;
