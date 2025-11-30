2a0d21ca44349e32e26baf63992428cd9080f22c
JUNBUNG-droid <rimblee2000@gmail.com>
2025-11-30 22:19:26 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251130/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    char c,d;
+    cin >> a >> c >> b >> d;
+    cout << ((a>=19 && c=='M')||b>=19 && d=='M');
+    return 0;
+}
