ffbdc4cb0204acfcefcedaa917f3cbd3e56e8a59
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-09 22:54:11 +0900

    Title: 20대의 평균 나이, Time: 1083ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 251209/20대의 평균 나이/average-age-of-20.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int sum=0,cnt=0;
+    while(true){
+        int age;
+        cin >> age;
+        if(age>=30){
+            break;
+        }
+        sum+=age;
+        cnt++;
+    }
+    cout << fixed;
+    cout.precision(2);
+    cout << (double)sum/cnt;
+    return 0;
+}
