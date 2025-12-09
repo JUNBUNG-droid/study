f11146f75a872680c85056534fb6cc4f8024edd4
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-09 22:29:00 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251209/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while(true){
+        int a;
+        cin >> a;
+        if(a==0){
+            break;
+        }
+        cout << a << "\n";
+    }
+    return 0;
+}
