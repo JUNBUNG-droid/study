c66676f4224c91cb532407f499600025028e52e6
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-12 18:48:20 +0900

    Title: 별표 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251212/별표 출력하기/print-star.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int k=1;
+	for (int i = 0; i < 2*n-1; i++) {
+		for (int j = 0; j < k; j++) {
+			cout << "*" << " ";
+		}
+		cout << endl;
+
+		if (i < n-1) {
+			k++;
+		}
+		else {
+			k--;
+		}
+	}
+
+	return 0;
+
+}
+
