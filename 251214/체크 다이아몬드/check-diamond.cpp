18778cc0e3607e617ef68a3bf31b7613bb2e3f32
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-14 19:01:24 +0900

    Title: 체크 다이아몬드, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251214/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>i; j--){
+            cout << " ";
+        }        
+        for(int j=0; j<i; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    for(int i=n; i>1; i--){
+        for(int j=n; j>i-1; j--){
+            cout << " ";
+        }          
+        for(int j=0; j<i-1; j++){
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
