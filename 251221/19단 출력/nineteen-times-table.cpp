a29d91c83992ea3576aae8c9d1f1764cc8bb82ca
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-21 23:59:00 +0900

    Title: 19단 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251221/19단 출력/nineteen-times-table.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for(int i=1; i<20; i++){
+        for(int j=1; j<20; j++){
+            if(j%2==0){
+                cout << i << " * " << j << " = " << i*j << "\n";
+            }
+            else{
+                if(j<19){
+                    cout << i << " * " << j << " = " << i*j << " / ";   
+                }
+                else{
+                    cout << i << " * " << j << " = " << i*j;  
+                }             
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
