713d7c3293da7dc7be7e9c98acb683da20861800
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-22 18:14:00 +0900

    Title: 약수가 세 개인 수, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251222/약수가 세 개인 수/numbers-with-three-divisors.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int st, ed, total=0;
+
+int main() {
+    cin >> st >> ed;
+
+    // Please write your code here.
+    for(int i=st; i<=ed; i++){
+        int cnt=0;
+        for(int j=1; j<=i; j++){
+            if(i%j==0){
+                cnt++;
+            }
+        }
+        if(cnt==3){
+            total++;
+        }
+    }
+    cout << total;
+
+    return 0;
+}
