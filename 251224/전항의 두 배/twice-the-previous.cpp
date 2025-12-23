b179fa123b396be2da63355bad215e8acf50a239
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-24 00:31:50 +0900

    Title: 전항의 두 배, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10], a, b;
+    cin >> a >> b;
+    arr[0]=a;
+    arr[1]=b;
+    for(int i=2; i<10; i++){
+        arr[i]=arr[i-1]+2*arr[i-2];
+    }
+    for(int i=0; i<10; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
