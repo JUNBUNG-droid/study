e6a61586cf9035ba04b1bb466c60a09f978faa4e
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-23 19:53:31 +0900

    Title: 평균점수 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251223/평균점수 구하기/print-average.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double arr[10], sum=0;
+    for(int i=0; i<8; i++){
+        cin >> arr[i];
+        sum+=arr[i];
+    }
+    cout << sum/8;
+    return 0;
+}
