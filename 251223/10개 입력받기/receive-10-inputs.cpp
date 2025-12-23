2fc0a9fd1624f9954439782da7a96e2fe176fad3
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-23 20:14:01 +0900

    Title: 10개 입력받기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251223/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10],cnt=0,sum=0;
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+        if(arr[i]==0){
+            break;
+        }
+        sum+=arr[i];
+        cnt++;
+    }
+    cout << fixed;
+    cout.precision(1);
+    cout << sum << " " << (double)sum/cnt;
+    return 0;
+}
