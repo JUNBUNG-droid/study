fe836dd5e1f1872c7b62cb2cdbd0218cb58ad74a
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-24 14:09:07 +0900

    Title: 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/개수 세기/count-numbers.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n, m, arr[100]={}, cnt=0;
+    cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+        if(arr[i]==m){
+            cnt++;
+        }
+	}
+
+	cout << cnt;
+	return 0;
+
+}
