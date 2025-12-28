6491f36fadf8d00903dd1f8e397ed1809ee82503
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-28 22:34:48 +0900

    Title: 두 수의 차의 최솟값, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251228/두 수의 차의 최솟값/minimum-difference-between-two-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int a[10];
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    int b=a[1]-a[0];
+    for(int i=1; i<n-1; i++){
+        if(a[i+1]-a[1]<b){
+            b=a[i+1]-a[1];
+        }
+    }
+    cout << b;
+    return 0;
+}
