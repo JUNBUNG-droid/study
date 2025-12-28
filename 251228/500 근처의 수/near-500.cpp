3065e527fe97c708175ec8bfd4bc08dc15b614b0
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-28 21:28:35 +0900

    Title: 500 근처의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251228/500 근처의 수/near-500.cpp
+++ 251228/500 근처의 수/near-500.cpp
@@ -9,22 +9,26 @@ int main() {
         cin >> a[i];
     }
 
-    int max=0;
+    int max=0, min=0;
     for(int i=1; i<10; i++){
         if(a[i]>a[max]){
             max=i;
         }
     }
-
-    int max_idx=0, min_idx=0;
     for(int i=1; i<10; i++){
-        if(a[i]>a[max_idx]&&500>a[i]){
-            max_idx=i;
-        }
-        if(a[i]<a[max]&&500<a[i]){
-            min_idx=i;
+        if(a[i]<a[min]){
+            min=i;
         }
     }
-    cout << a[max_idx] << " " << a[min_idx];
+
+    for(int i=0; i<10; i++){
+        if(a[i]>a[min]&&500>a[i]){
+            min=i;
+        }
+        if(a[i]<a[max]&&500<a[i]){
+            max=i;
+        }
+    }
+    cout << a[min] << " " << a[max];
     return 0;
 }
