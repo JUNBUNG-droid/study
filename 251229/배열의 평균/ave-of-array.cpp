3731ca35bccdd1b6524b6446a50297367021b51b
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-29 01:37:39 +0900

    Title: 배열의 평균, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251229/배열의 평균/ave-of-array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[2][4];
+    for(int i=0; i<2; i++){
+        for(int j=0; j<4; j++){
+            cin >> arr[i][j];
+        }
+    }
+    cout << fixed;
+    cout.precision(1);
+    for(int i=0; i<2; i++){
+        double sum=0;
+        for(int j=0; j<4; j++){
+            sum+=arr[i][j];
+        }
+        cout << sum/4 << " ";
+    }
+    cout << "\n";
+    for(int i=0; i<4; i++){
+        double sum=0;
+        for(int j=0; j<2; j++){
+            sum+=arr[j][i];
+        }
+        cout << sum/2 << " ";
+    }
+    cout << "\n";
+    double sum=0;
+    for(int i=0; i<2; i++){
+        for(int j=0; j<4; j++){
+            sum+=arr[i][j];
+        }
+    }
+    cout << sum/8 << " ";
+    return 0;
+}
