4c5555160cf3343b63977abd01cf8d0501593c80
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-29 22:50:10 +0900

    Title: 파스칼의 삼각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251229/파스칼의 삼각형/pascals-triangle.cpp
+++ 251229/파스칼의 삼각형/pascals-triangle.cpp
@@ -13,7 +13,7 @@ int main() {
     }
 
     for(int i=2; i<n; i++){
-        for(int j=1; j<i+1; j++){
+        for(int j=1; j<i; j++){
             arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
         }
     }
