d78ab7b9e4103a052c10317cd68df8995c442f17
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-30 00:02:36 +0900

    Title: 문자열의 길이 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251230/문자열의 길이 출력하기/print-strings-length.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a;
+    string b;
+    cin >> a >> b;
+    cout << a.length()+b.length();
+    return 0;
+}
