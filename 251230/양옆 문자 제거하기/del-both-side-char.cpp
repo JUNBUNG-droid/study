f83d9e14e4651850167a6bb3de98edb7dd7efa85
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-30 22:20:05 +0900

    Title: 양옆 문자 제거하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251230/양옆 문자 제거하기/del-both-side-char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    str.erase(1,1);
+    str.erase(str.length()-2,1);
+    cout << str;
+    return 0;
+}
