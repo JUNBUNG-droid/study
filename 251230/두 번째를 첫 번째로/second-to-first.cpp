55453dd53e89de0b1a09cb2ff6917c02cb1bf067
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-30 22:13:26 +0900

    Title: 두 번째를 첫 번째로, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251230/두 번째를 첫 번째로/second-to-first.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    char a=str[0];
+    char b=str[1];
+    for(int i=0; i<str.length(); i++){
+        if(str[i]==b){
+            str[i]=a;
+        }
+    }
+    cout << str;
+    return 0;
+}
