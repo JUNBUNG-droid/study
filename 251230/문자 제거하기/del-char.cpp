1fa447be972ac8582b096bcb5d494d703a0ef5ca
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-30 22:26:13 +0900

    Title: 문자 제거하기, Time: 1094ms, Memory: 36MB, Status: Time Limit Exceed - Codetree

---
+++ 251230/문자 제거하기/del-char.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    while(str.length()>1){
+        int n;
+        cin >> n;
+        if(n>str.length()){
+            n=str.length()-1;
+        }
+        str.erase(n,1);
+        cout << str << "\n";
+    }
+    return 0;
+}
