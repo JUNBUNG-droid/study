15fc6d931d2b4437518c0ecef8479cea2aacbebf
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-30 23:23:39 +0900

    Title: e 제거하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251230/e 제거하기/e-to-remove.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    for(int i=0; i<str.length(); i++){
+        if(str[i]=='e'){
+            str.erase(i,1);
+            break;
+        }
+    }
+    cout << str;
+    return 0;
+}
