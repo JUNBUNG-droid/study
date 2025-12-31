8cb24817a8f8b990f90c6101cd6e08845cea6328
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-31 18:30:50 +0900

    Title: 두 수의 합과 1, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251231/두 수의 합과 1/two-nums-sum-and-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int a, b;
+    cin >> a >> b;
+    int cnt=0;
+
+	string a_str = to_string(a+b);
+    for(int i=0; i<a_str.length(); i++){
+        if(a_str[i]=='1'){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+	return 0;
+
+}
+
+
