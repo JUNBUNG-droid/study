2b3d32426585ba4007b1e3339fc8d9f75fabf8f4
JUNBUNG-droid <rimblee2000@gmail.com>
2025-12-31 18:16:52 +0900

    Title: 대문자 소문자 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251231/대문자 소문자 바꾸기/change-uppercase-and-lowercase.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string str;
+    cin >> str;
+    int len = str.length();
+    for(int i=0; i<len; i++){
+        if(str[i]>='A'&&str[i]<='Z'){
+            cout << (char)tolower(str[i]);
+        }
+        else if(str[i]>='a'&&str[i]<='z'){
+            cout << (char)toupper(str[i]);
+        }
+    }
+    return 0;
+}
