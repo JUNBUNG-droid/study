6f5139595886af355eb1193a1868a52ab7dac11f
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-01 00:40:33 +0900

    Title: 최소공배수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260101/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+void cal(int n, int m){
+    int a;
+    if(n>m){
+        a=m;
+    }
+    else{
+        a=n;
+    }
+    int b;
+    for(int i=1; i<=a; i++){
+        if(n%i==0&&m%i==0){
+            b=i;
+        }
+    }
+    cout << b*(n/b)*(m/b);
+}
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+    cal(n,m);
+
+    return 0;
+}
