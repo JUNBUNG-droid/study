f20c9ab83198e107e9e44774c0034059c6ebd8d9
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-02 22:42:30 +0900

    Title: 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260102/정수의 최솟값/minimum-value-of-an-integer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+void d(int a, int b, int c){
+    if(a<b&&a<c){
+        cout << a;
+    }
+    else if(b<a&&b<c){
+        cout << b;
+    }
+    else if(c<a&&c<b){
+        cout << c;
+    }
+}
+
+int main() {
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    d(a,b,c);
+
+    return 0;
+}
