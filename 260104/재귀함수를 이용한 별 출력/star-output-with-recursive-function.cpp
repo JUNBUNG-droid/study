865e246397102451c26b35ffd5f79be0584d1441
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-04 16:12:45 +0900

    Title: 재귀함수를 이용한 별 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void printstar(int n){
+    if(n==0){
+        return;
+    }
+    printstar(n-1);
+    for(int i=0; i<n; i++){
+        cout << '*';
+    }
+    cout << "\n";
+}
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    printstar(n);
+
+    return 0;
+}
