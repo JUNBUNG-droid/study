06b05846e9f69dca9db9e75d5dd3b0050d7202fa
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-04 23:11:27 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 1095ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

---
+++ 260104/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int a(int n){
+    if(n==1){
+        return 0;
+    }
+    if(n%2==0){
+        n/=2;
+        a(n)+1;
+    }
+    else{
+        n*=3;
+        n++;
+        a(n)+1;
+    }
+    return a(n)+1;
+}
+
+int main() {
+    cin >> n;
+
+    // Please write your code here.
+    cout << a(n);
+
+    return 0;
+}
