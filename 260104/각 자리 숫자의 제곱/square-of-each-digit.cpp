c239400b511954c6e82b3d940013a67bf1a76027
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-04 17:41:17 +0900

    Title: 각 자리 숫자의 제곱, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int F(int n) {
+    if(n < 10)
+        return n*n;
+
+    return F(n / 10) + (n % 10)*(n % 10);
+}
+
+int main() {
+    cin >> N;
+    cout << F(N);
+    return 0;
+}
+
