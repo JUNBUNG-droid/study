0edf1a2303b6af61d2d8cfa3e44c1a2b83f0ddee
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-04 22:56:28 +0900

    Title: 큰 수 자리수의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260104/큰 수 자리수의 합/sum-of-large-numeric-digits.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int sum(int n){
+    if(n<10){
+        return n;
+    }
+    return sum(n/10)+n%10;
+}
+
+int main() {
+    cin >> a >> b >> c;
+
+    // Please write your code here.
+    cout << sum(a*b*c);
+
+    return 0;
+}
