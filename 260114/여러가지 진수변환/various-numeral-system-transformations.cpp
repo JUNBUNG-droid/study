0fd479ea1e6cb13fa3b33feab5e8854566fac9c9
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-14 21:51:27 +0900

    Title: 여러가지 진수변환, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260114/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int N, B;
+
+void a(int n, int b){
+    string a = "";
+    while(true){
+        if(n<2){
+            a+=to_string(n);
+            break;
+        }
+        a+=to_string(n%b);
+        n/=b;
+    }
+    for(int i=a.length()-1; i>=0; i--){
+        cout << a[i];
+    }
+}
+
+int main() {
+    cin >> N >> B;
+
+    // Please write your code here.
+    a(N,B);
+
+    return 0;
+}
