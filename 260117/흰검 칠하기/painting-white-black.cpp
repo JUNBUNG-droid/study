1495959b3fbe8e773bd40eb4b635ccf52779f46b
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-17 20:09:06 +0900

    Title: 흰검 칠하기, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260117/흰검 칠하기/painting-white-black.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+char dir;
+
+class Info{
+    public:
+        int idx;
+        char color;
+
+    Info(int idx=0, char color=' '){
+        this->idx=idx;
+        this->color=color;
+    }
+};
+
+Info x[300];
+
+int main() {
+    cin >> n;
+    int point = 150;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a >> dir;
+        if(dir=='R'){
+            for(int j = 0; j<a; j++){
+                x[point].idx++;
+                if(x[point].idx>=4){
+                    x[point].color='G';
+                }
+                else{
+                    x[point].color='B';
+                }
+                point++;
+            }
+            point--;
+        }
+        else if(dir=='L'){
+            for(int j = 0; j<a; j++){
+                x[point].idx++;
+                if(x[point].idx>=4){
+                    x[point].color='G';
+                }
+                else{
+                    x[point].color='W';
+                }
+                point--;
+            }
+            point++;
+        }
+    }
+    int cntb=0, cntw=0, cntg=0;
+    for(int i=0; i<300; i++){
+        if(x[i].color=='B'){
+            cntb++;
+        }
+        if(x[i].color=='G'){
+            cntg++;
+        }
+        if(x[i].color=='W'){
+            cntw++;
+        }
+    }
+    cout << cntw << " " << cntb << " " << cntg;
+    return 0;
+}
