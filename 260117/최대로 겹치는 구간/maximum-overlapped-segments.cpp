9231c15c55087032ec5e3e2d1b0caf13c731d439
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-17 17:42:38 +0900

    Title: 최대로 겹치는 구간, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260117/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 260117/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int line[100]={};
+int line[200]={};
 
 int max_h(){
     int max = line[0];
-    for(int i=1; i<100; i++){
+    for(int i=1; i<200; i++){
         if(max<line[i]){
             max=line[i];
         }
@@ -19,14 +19,12 @@ int main() {
     cin >> N;
 
     for(int i=0; i<N; i++){
-        int x1, x2;
+        int x1, x2, move;
         cin >> x1 >> x2;
-        if(x1<0){
-            x1-=x1;
-            x2-=x1;
-        }
+        x1+=100;
+        x2+=100;
         for(int j=x1; j<x2; j++){
-            line[j-1]++;
+            line[j]++;
         }
     }
     cout << max_h();
