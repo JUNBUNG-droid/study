4f0527f60af655d16e1b2d19d7ec7c1f3235a41c
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-20 21:53:51 +0900

    Title: 사각형의 총 넓이 2, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260120/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
+++ 260120/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -3,23 +3,54 @@
 using namespace std;
 
 int N;
-int x[10][10]={};
+int x1[10], y1[10];
+int x2[10], y2[10];
+int x[200][200]={};
+
 int main() {
     cin >> N;
-    int x1, y1, x2, y2;
+
     for (int i = 0; i < N; i++) {
-        cin >> x1 >> y1 >> x2 >> y2;
-        for(int j=x1; j<x2; j++){
-            for(int k=y1; k<y2; k++){
+        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+    }
+
+    // Please write your code here.
+    int minx1=0, minx2=0, miny1=0, miny2=0;
+    for(int i=0; i<N; i++){
+        if(minx1>x1[i]){
+            minx1=x1[i];
+        }
+        if(minx2>x2[i]){
+            minx2=x2[i];
+        }
+        if(miny1>y1[i]){
+            miny1=y1[i];
+        }
+        if(miny2>y2[i]){
+            miny2=y2[i];
+        }
+    }
+    int min;
+    if(minx1>minx2){
+        min=minx2;
+    }
+    if(minx2>miny1){
+        min=miny1;
+    }
+    if(miny1>miny2){
+        min=miny2;
+    }
+
+    for(int i=0; i<N; i++){
+        for(int j=x1[i]-min; j<x2[i]-min; j++){
+            for(int k=y1[i]-min; k<y2[i]-min; k++){
                 x[j][k]=1;
             }
         }
     }
-
     int cnt=0;
-    // Please write your code here.
-    for(int i=0; i<10; i++){
-        for(int j=0; j<10; j++){
+    for(int i=0; i<200; i++){
+        for(int j=0; j<200; j++){
             if(x[i][j]==1){
                 cnt++;
             }
