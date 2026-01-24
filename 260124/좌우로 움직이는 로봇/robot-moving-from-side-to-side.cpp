81e9de07382a9a00e0e01461ecdaba0a421734c3
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-24 17:25:32 +0900

    Title: 좌우로 움직이는 로봇, Time: 32ms, Memory: 8MB, Status: Wrong Answer - Codetree

---
+++ 260124/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int a[1000000]={};
+int b[1000000]={};
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+    int apoint=1;
+    for(int i=0; i<n; i++){
+        int t;
+        char d;
+        cin >> t >> d;
+        while(t--){
+            if(d=='R'){
+                a[apoint]=a[apoint-1]+1;
+            }
+            else if(d=='L'){
+                a[apoint]=a[apoint-1]-1;
+            }
+            apoint++;
+        }
+    }
+    int bpoint=1;
+    for(int i=0; i<m; i++){
+        int t;
+        char d;
+        cin >> t >> d;
+        while(t--){
+            if(d=='R'){
+                b[bpoint]=b[bpoint-1]+1;
+            }
+            else if(d=='L'){
+                b[bpoint]=b[bpoint-1]-1;
+            }
+            bpoint++;
+        }
+    }
+    
+    int start=min(apoint, bpoint);
+    int goal=max(apoint, bpoint);
+
+    for(int i=start; i<goal; i++){
+        if(apoint<bpoint){
+            a[i]=a[apoint-1];
+        }
+        else if(apoint>bpoint){
+            b[i]=b[bpoint-1];
+        }
+    }
+
+    int cnt=0;
+    for(int i=1; i<goal; i++){
+        if(a[i]==b[i]&&a[i-1]!=b[i-1]){
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
