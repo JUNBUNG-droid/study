c7bdd7b9cd44a9731e70eaea7d7bd168a167ec6f
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-24 15:32:36 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260124/벌금은 누구에게/who-will-pay.cpp
+++ 260124/벌금은 누구에게/who-will-pay.cpp
@@ -14,15 +14,11 @@ int main() {
     }
 
     // Please write your code here.
-    for(int i=1; i<=N; i++){
-        for(int j=0; j<M; j++){
-            if(student[j]==i){
-                record[i]++;
-            }
-            if(record[i]>=K){
-                cout << i;
-                return 0;
-            }
+    for(int i=0; i<M; i++){
+        record[student[i]]++;
+        if(record[student[i]]==K){
+            cout << student[i];
+            return 0;
         }
     }
     cout << -1;
