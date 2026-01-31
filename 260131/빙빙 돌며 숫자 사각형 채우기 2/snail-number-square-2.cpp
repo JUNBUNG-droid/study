e68dbd29ab63dde43276e6a5e60a1f226e96e94f
JUNBUNG-droid <rimblee2000@gmail.com>
2026-01-31 21:13:56 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260131/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[100][100];
+int used[100][100]={};
+
+int dx[4]={1,0,-1,0};
+int dy[4]={0,1,0,-1};
+
+bool isrange(int x, int y){
+    return (0<=x&&x<m)&&(0<=y&&y<n);
+}
+
+int main() {
+    cin >> n >> m;
+
+    // Please write your code here.
+    int cnt=1, x=0, y=0;
+    int dir_num=1;
+    arr[y][x]=cnt++;
+    used[y][x]=true;
+
+    for(int i=1; i<n*m; i++){
+        int nx=x+dx[dir_num];
+        int ny=y+dy[dir_num];
+        if(!(isrange(nx,ny)&&used[ny][nx]==false)){
+            dir_num=(dir_num+3)%4; 
+        }
+        x+=dx[dir_num];
+        y+=dy[dir_num];
+        used[y][x]=true;
+        arr[y][x]=cnt++;   
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
