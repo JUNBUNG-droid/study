b6ae928ee5e0f62a376505ba3ef1b96752912c1e
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-01 21:31:32 +0900

    Title: 체크판위에서 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260201/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int R, C;
+char grid[15][15];
+
+int main() {
+    cin >> R >> C;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // Please write your code here.
+    int cnt=0;
+    for(int i=1; i<R-2; i++){
+        for (int j=0; j<C-2; j++) {
+            for(int k=i+1; k<R-1; k++){
+                for(int l=j+1; l<C-1; l++){
+                    if(grid[0][0]!=grid[i][j]&&grid[i][j]!=grid[k][l]) cnt++;
+                }
+            }
+        }        
+    }
+    cout << cnt;
+    return 0;
+}
