689467bd568280e4d715488203248f996a1baae4
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-03 09:07:14 +0900

    Title: 씨 오 더블유 2, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/씨 오 더블유 2/c-o-w-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n;
+string str;
+
+int main() {
+    cin >> n;
+    cin >> str;
+
+    // Please write your code here.
+    int cnt=0;
+    for(int i=0; i<str.length(); i++){
+        if(str[i]=='C'){
+            for(int j=i+1; j<str.length(); j++){
+                if(str[j]=='O'){
+                    for(int k=j+1; k<str.length(); k++){
+                        if(str[k]=='W') cnt++;
+                    }
+                }
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
