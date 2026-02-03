d053f5b86c681c4a62459b1bd9a0f163bcba8879
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-03 16:49:29 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260203/최고의 13위치 2/best-place-of-13-2.cpp
+++ 260203/최고의 13위치 2/best-place-of-13-2.cpp
@@ -31,11 +31,20 @@ int main() {
         }
     }
     else{
+        for(int k=0; k<N; k++){
+            int a=0;
+            for(int j=0; j<N-5; j++){
+                a+=arr[k][j]+arr[k][j+1]+arr[k][j+2];
+                for(int i=j+3; i<N-2; i++){
+                    a+=arr[k][i]+arr[k][i+1]+arr[k][i+2];
+                }
+            }
+            if(max<a) max=a;
+        }
         for(int i=0; i<N; i++){
             for(int j=0; j<N-2; j++){
-                if(i==N-1&&j+5==N) break;
-                for(int k=0; k<N; k++){
-                    for(int l=3; l<N-2; l++){
+                for(int k=i+1; k<N; k++){
+                    for(int l=0; l<N-2; l++){
                         int sum=0;
                         sum+=(arr[i][j]+arr[i][j+1]+arr[i][j+2]);
                         sum+=(arr[k][l]+arr[k][l+1]+arr[k][l+2]);
