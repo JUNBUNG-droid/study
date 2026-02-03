12ad4115e67e55f6d67f88ee50eac4c3168a14c8
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-03 16:54:54 +0900

    Title: 최고의 13위치 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260203/최고의 13위치 2/best-place-of-13-2.cpp
+++ 260203/최고의 13위치 2/best-place-of-13-2.cpp
@@ -9,54 +9,41 @@ int arr[20][20];
 int main() {
     cin >> N;
     for (int i = 0; i < N; i++)
-        for (int j = 0; j < N; j++) cin >> arr[i][j];
+        for (int j = 0; j < N; j++)
+            cin >> arr[i][j];
 
-    // Please write your code here.
-    int max=INT_MIN;
-    if(N<6){
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N-2; j++){
-                for(int k=i+1; k<N; k++){
-                    for(int l=0; l<N-2; l++){
-                        int sum=0;
-                        sum+=(arr[i][j]+arr[i][j+1]+arr[i][j+2]);
-                        sum+=(arr[k][l]+arr[k][l+1]+arr[k][l+2]);
-                        if(max<sum){
-                            max=sum;
-                            sum=0;
-                        } 
+    int max = INT_MIN;
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N-2; j++){
+            for(int k = i+1; k < N; k++){
+                for(int l = 0; l < N-2; l++){
+                    int sum = 0;
+                    sum += arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                    sum += arr[k][l] + arr[k][l+1] + arr[k][l+2];
+
+                    if(max < sum){
+                        max = sum;
                     }
                 }
             }
         }
     }
-    else{
-        for(int k=0; k<N; k++){
-            int a=0;
-            for(int j=0; j<N-5; j++){
-                a+=arr[k][j]+arr[k][j+1]+arr[k][j+2];
-                for(int i=j+3; i<N-2; i++){
-                    a+=arr[k][i]+arr[k][i+1]+arr[k][i+2];
-                }
-            }
-            if(max<a) max=a;
-        }
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N-2; j++){
-                for(int k=i+1; k<N; k++){
-                    for(int l=0; l<N-2; l++){
-                        int sum=0;
-                        sum+=(arr[i][j]+arr[i][j+1]+arr[i][j+2]);
-                        sum+=(arr[k][l]+arr[k][l+1]+arr[k][l+2]);
-                        if(max<sum){
-                            max=sum;
-                            sum=0;
-                        } 
-                    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N-2; j++){
+            for(int k = j+3; k < N-2; k++){
+                int sum = 0;
+                sum += arr[i][j] + arr[i][j+1] + arr[i][j+2];
+                sum += arr[i][k] + arr[i][k+1] + arr[i][k+2];
+
+                if(max < sum){
+                    max = sum;
                 }
             }
         }
-    }        
+    }
+
     cout << max;
     return 0;
 }
