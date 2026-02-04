6c9b50628a21e9e17283aa4a826c0ee84be269b7
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-04 10:41:50 +0900

    Title: 특정 구간의 원소 평균값, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 260204/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 260204/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -14,15 +14,13 @@ int main() {
     // Please write your code here.
     int ans=0;
     for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
+        for(int j=i; j<n; j++){
             bool istrue = false;
-            int sum=0;
-            int cnt=0;
+            double sum=0;
             for(int k=i; k<=j; k++){
                 sum+=arr[k];
-                cnt++;
             }
-            int avg=sum/cnt;
+            double avg=sum/(j-i+1);
             for(int k=i; k<=j; k++){
                 if(avg==arr[k]) istrue=true;
             }
