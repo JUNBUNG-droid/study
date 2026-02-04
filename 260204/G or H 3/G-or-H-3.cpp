7d80605d6e9e5a9fd4e71dbfaea741f1202f3c8e
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-04 10:13:41 +0900

    Title: G or H 3, Time: 113ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260204/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int N, K;
+int x[100];
+char c[10000];
+
+int main() {
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> x[i] >> c[x[i]];
+    }
+
+    // Please write your code here.
+    int max_val=INT_MIN;
+    for(int i=1; i<=9993; i++){
+        int score=0;
+        for(int j=i; j<i+K+1; j++){
+            if(c[j]=='G') score++;
+            else if(c[j]=='H') score+=2; 
+        }
+        max_val=max(max_val, score);
+    }
+    cout << max_val;
+    return 0;
+}
