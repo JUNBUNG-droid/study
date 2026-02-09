48091281240c22ebbda59c3bf7084edde9b577e2
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-09 21:40:47 +0900

    Title: 이상한 폭탄 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260209/이상한 폭탄 2/strange-bomb-2.cpp
+++ 260209/이상한 폭탄 2/strange-bomb-2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int N, K;
-int num[100];
+int num[201]={};
 
 int main() {
     cin >> N >> K;
@@ -15,7 +15,7 @@ int main() {
 
     //폭탄 하나 정하기
     int max_num=-1;
-    for(int i=0; i<N-K; i++){
+    for(int i=0; i<N; i++){
         //거리 내 폭탄 확인
         for(int j=1; j<=K; j++){
             if(num[i]==num[i+j]) max_num=max(max_num, num[i+j]);
