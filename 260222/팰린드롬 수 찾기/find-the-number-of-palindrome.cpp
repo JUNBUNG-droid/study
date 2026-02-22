0d761da0c16782d556591c40cb179fc424c6a665
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-22 17:26:40 +0900

    Title: 팰린드롬 수 찾기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260222/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int X, Y;
+
+int main() {
+    cin >> X >> Y;
+
+    // Please write your code here.
+    int cnt=0;
+    for(int i=X; i<=Y; i++){
+        string str=to_string(i);
+        bool check = true;
+        for(int j=0; j<str.length()/2; j++){
+            if(str[j]!=str[str.length()-j-1]) check = false;
+            if(check) cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
