dd3710f364bf108b4995de481355f0ce19e2209b
JUNBUNG-droid <rimblee2000@gmail.com>
2026-02-24 10:52:43 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260224/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int min_val=1000;
+    // *2 숫자 정하기
+    for(int i=0; i<n; i++){
+        arr[i]*=2;
+        // 숫자 하나 제외
+        for(int j=0; j<n; j++){
+            int current[4]={}, idx=0;
+            for(int k=0; k<n; k++){
+                if(j==k) continue;
+                current[idx++] = arr[k];
+            }
+
+            int diff_sum=0;
+            for(int k=0; k<n-2; k++){
+                diff_sum+=abs(current[k]-current[k+1]);
+            }
+
+            if(min_val>diff_sum) min_val = diff_sum;
+        }
+        arr[i]/=2;
+    }
+
+    cout << min_val;
+
+    return 0;
+}
