4e35bad8a323d1d4ad0f389131e0b177167e9754
JUNBUNG-droid <rimblee2000@gmail.com>
2026-03-11 14:52:40 +0900

    Title: 아름다운 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260311/아름다운 수/beautiful-number.cpp
+++ 260311/아름다운 수/beautiful-number.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int n;
+vector<int> v;
 
 int choose(int a) {
-    if (a == n) return 1;   
-    if (a > n) return 0;    
+    if (a == n) return 1;
+
+    if (a > n) return 0;
 
     int ans = 0;
-    for (int i = 1; i <= n; i++) {
+
+    for (int i = 1; i <= 4; i++) {
+        for (int j = 0; j < i; j++) {
+            v.push_back(i);
+        }
+
         ans += choose(a + i);
+
+        for (int j = 0; j < i; j++) {
+            v.pop_back();
+        }
     }
+
     return ans;
 }
 
