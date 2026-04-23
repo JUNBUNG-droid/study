b2e7779250c99d2e251040d7387fa66b60de3164
JUNBUNG-droid <rimblee2000@gmail.com>
2026-04-23 23:01:00 +0900

    Title: 사다리 타기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260423/사다리 타기/ladder-game.cpp
+++ 260423/사다리 타기/ladder-game.cpp
@@ -1,59 +1,56 @@
 #include <iostream>
+#include <algorithm>
 #include <climits>
-
 using namespace std;
 
 int n, m;
-int origin[17][21];        
-int sub[17][21];           
-int lineR[11], lineC[11];    
-int originResult[11];      
+int line_c[11];                
+bool picked[11];            
+int origin_result[11];
 int ans = INT_MAX;
 
-void simulate(int ladder[17][21], int result[]) {
-    for (int s = 0; s < n; s++) {
-        int col = 2 * s;                  
-        for (int row = 1; row <= m; row++) {
-            if (col + 1 <= 2*n - 2 && ladder[row][col + 1] == 1)
-                col += 2;                   
-            else if (col - 1 >= 1 && ladder[row][col - 1] == 1)
-                col -= 2;                 
-        }
-        result[s] = col / 2;
+
+void simulate(int result[], bool use_all) {
+    for (int i = 0; i < n; i++) result[i] = i;
+    for (int i = 0; i < m; i++) {
+        if (!use_all && !picked[i]) continue;
+        swap(result[line_c[i]], result[line_c[i] + 1]);
     }
 }
 
 void solve(int idx, int cnt) {
-    if (cnt >= ans) return;                 
+    if (cnt >= ans) return;
 
     if (idx == m) {
-        int subResult[11];
-        simulate(sub, subResult);
+        int sub_result[11];
+        simulate(sub_result, false);
         for (int i = 0; i < n; i++)
-            if (subResult[i] != originResult[i]) return;
+            if (sub_result[i] != origin_result[i]) return;
         ans = cnt;
         return;
     }
 
-    sub[lineR[idx]][lineC[idx]] = 1;
+    picked[idx] = true;
     solve(idx + 1, cnt + 1);
-    sub[lineR[idx]][lineC[idx]] = 0;       
+    picked[idx] = false;
     solve(idx + 1, cnt);
 }
 
 int main() {
     cin >> n >> m;
 
+    pair<int,int> tmp[11];    
     for (int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
-        int r = b, c = 2 * a - 1;     
-        origin[r][c] = 1;
-        lineR[i] = r;
-        lineC[i] = c;
+        tmp[i] = {b, a - 1};
     }
+    sort(tmp, tmp + m);       
 
-    simulate(origin, originResult);         
+    for (int i = 0; i < m; i++)
+        line_c[i] = tmp[i].second;
+
+    simulate(origin_result, true);
     solve(0, 0);
 
     cout << ans;
